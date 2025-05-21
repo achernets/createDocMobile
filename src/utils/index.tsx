44adfecfde1d4d8b35ed7206946c35b3f4eb60dd
o.chernets <chernetsoleksandr.dev@gmail.com>
2025-05-21 14:49:27 +0300

    small fix

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -229,12 +229,12 @@ export const sendMessageMobile = (message, id: string | null) => {
   if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.callbackHandler) {
     // iOS (WKWebView) 
     //@ts-ignore
-    window.webkit.messageHandlers.callbackHandler.postMessage(message, id);
+    window.webkit.messageHandlers.callbackHandler.postMessage([message, id]);
     //@ts-ignore
   } else if (window.Android && window.Android.showMessage) {
     // Android (WebView)
     //@ts-ignore
-    window.Android.showMessage(message, id);
+    window.Android.showMessage([message, id]);
   } else {
     console.log("No native interface found");
   }
